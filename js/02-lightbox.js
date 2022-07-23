7ff7d4f887e6fd38d86a3b321d582ae3ca4995ae
Ivan Onofriichuk <ivanonofrijcuk@gmail.com>
2022-07-23 15:32:07 +0300

    Finish
    Виконав 7му роботу

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+
+const fn = imgCreate(galleryItems);
+listEl.insertAdjacentHTML("afterbegin", fn);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+function imgCreate(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" data-caption=${description}/>
+</a>`;
+    })
+    .join(" ");
+}
